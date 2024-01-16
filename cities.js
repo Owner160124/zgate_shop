7395efc286a20aa6a773725f1923ec1f52a51564
Taras Stukalo <taras.frbslist@gmail.com>
2024-01-16 15:57:13 +0200

    fix firefox json error

--- cities.js
+++ cities.js
@@ -1,9 +1,13 @@
-import citiesList from './cities.json' assert { type: "json" };
+//import citiesList from './cities.json' assert { type: "json" };
 
 const dataList = document.getElementById('citiesData')
 
-citiesList.forEach(city => {
+fetch('./cities.json')
+.then( res => res.json())
+.then(data => data.forEach(city => {
   const el = document.createElement('option')
   el.setAttribute('value',`${city.name}, ${city.state}`)
   dataList.appendChild(el)
-});
+}))
+
+
